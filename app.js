bfd4d55913957a17de1b14245e134fecb1d5e67e
jagadeesedith <tiktokulagam91@gmail.com>
2026-02-20 01:26:06 -0800

    first comment

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const IMG = {
     oniondosa: 'images/onion dosa.png',
 
     poratta: 'images/poratta.jpg',
-    kothuporatta: 'images/kothuparotta.png',
+    kothuporatta: 'images/kothuparotta.jpg',
 
     halfboiled: 'images/halfboil.png',
     omelette: 'images/ombelette.jpg',
